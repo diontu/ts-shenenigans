c8a96f225bd62887867550a81ce51ea8591a904d
diontugit <diontugit@gmail.com>
2024-01-07 12:46:07 -0500

    minor: add anyunknownundefineandnull code

---
+++ typescript/6-anyunknownsundefinedandnull.ts
@@ -0,0 +1,13 @@
+// any
+let anyVariable: any = [""];
+anyVariable.trim();
+
+// unknown (type-safe version of any)
+let unknownVariable: unknown = ["hello world"];
+console.log(unknownVariable);
+
+// undefined
+let undefinedVariable: undefined = undefined;
+
+// null
+let nullVariable: null = null;
