e942dba56f3ff00fb70ac623871df518762697bf
diontugit <diontugit@gmail.com>
2024-01-07 11:58:37 -0500

    minor: added typesints cdoe

---
+++ typescript/2-typesints.ts
@@ -0,0 +1,19 @@
+// primitive: string, number, boolean
+const someString: string = "";
+
+const someNumber: number = 321;
+
+const someBoolean: boolean = false;
+
+// other primitives: bigint, symbol
+
+// non-primitive
+const someFunction: () => number = () => 3;
+
+const someArray: number[] = [3, 2, 1];
+
+const someObject: { greeting: string } = {
+  greeting: "Hello World!",
+};
+
+// many more way to define types
