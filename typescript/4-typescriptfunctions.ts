c67f2fab322b34f9d9b3fb05dad0d0b0a1485266
diontugit <diontugit@gmail.com>
2024-01-07 12:20:32 -0500

    minor: add typescriptfunctions code

---
+++ typescript/4-typescriptfunctions.ts
@@ -0,0 +1,12 @@
+// function type expression
+const someFunction: (param: number) => number = (param) => param;
+
+// parameter type annotation
+const exampleOne = (param: number) => {
+  // do something...
+};
+
+// return type annotation
+const exampleTwo = (param: number): number => {
+  return param;
+};
